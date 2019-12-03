7358e55e1412d7ba9a4fa1d2f8eb42e049f9aa33
jjs3344 <jake.smith@bigcommerce.com>
2019-12-03 16:58:38 -0600

    fix loop

--- routes/system-logs/logs.js
+++ routes/system-logs/logs.js
@@ -1,41 +1,42 @@
 const v3 = require('../../config/BC').v3;
-const lupus = require('lupus');
 
 class SystemLogs {
     constructor(){
         this.v3 = v3
         this.state = {
-            current_page: 0,
-            next_page: 1
+            current_page: 1300,
+            next_page: 1301
         }
-        this.totalPages = 1382
+        this.totalPages = 85
         this.logs = [];
         this.meta = {};
-        this.getData = this.getData.bind(this)
     }
     async initClass() {
         await this.getMeta();
         await this.getData();
     }
     systemLogs() {
-        return this.v3.get(`/store/systemlogs?severity=4&page=${this.state.next_page}`)
+        return this.v3.get(`/store/systemlogs?severity=4&type=payment&limit=20&page=${this.state.next_page}`)
     }
 
     getMeta() {
         return (async () => {
             const { meta } = await this.systemLogs();
             this.meta = meta;
+            this.state.current_page = this.meta.pagination.current_page;
             return meta;
         })();
     }
+
     async currentPage() {
         const { pagination } = await this.getMeta();
         const { data } = await this.systemLogs()
 
         await data.map(x => {
             const dateFilter = x.date_created.split('T')[0]
-            if(dateFilter === '2019-06-18'){
+            if(dateFilter === '2019-12-02'){
                 this.logs.push(x)
+                console.log(x.id)
             }
         })
 
@@ -47,12 +48,10 @@ class SystemLogs {
 
     async getData() {
         const $this = this;
-        lupus(1, this.totalPages, async function(n) {
-            await $this.currentPage()
-            console.log("We're on:", n);
-          }, function() {
-            console.log('All done!');
-          });
+      for (let index = 0; index < this.totalPages; index++) {
+        await $this.currentPage()
+        console.log("We're on:", $this.state.next_page);
+      }
     }
 
 
